edfceb45e678d90f359f7c6ec7e4519cd1feb1de
Philodasha <damakor1007@gmail.com>
2022-10-17 16:11:11 +0300

    corrections

--- lesson6.py/main.py
+++ lesson6.py/main.py
@@ -3,7 +3,10 @@ count = 0
 sum_ = 0
 while count < n:
     grade = int(input('Как вы оцениваете работу нашего магазина?'))
-    sum_ += grade
-    count += 1
+    if grade > 5 or grade < 0:
+        print('Выставляей оценки от 1 до 5 включительно!')
+    else:
+        sum_ += grade
+        count += 1
 
 print('Средняя оценка: ', sum_/n)
